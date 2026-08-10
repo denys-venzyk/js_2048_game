e4d371dd8f69d7f061e82a0d52963f07a2997e11
Denys Venzyk <denys.venzyk@gmail.com>
2026-08-10 19:08:56 +0300

    got all elements from DOM, implemented functions setButtonToRestart and setButtonToStart

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,30 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const scoreEl = document.querySelector('.game-score');
+const startButton = document.querySelector('.button.start');
+const winMessage = document.querySelector('.message-start');
+const loseMessage = document.querySelector('.message-lose');
+const startMessage = document.querySelector('.message-start');
+const fieldRows = document.querySelector('.field-row');
+
+let hasMoved = false;
+
+function setButtonToRestart() {
+  startButton.textContent = 'Restart';
+  startButton.classList.remove('start');
+  startButton.classList.add('restart');
+}
+
+function setButtonToStart() {
+  startButton.textContent = 'Start';
+  startButton.classList.remove('restart');
+  startButton.classList.add('start');
+}
+
+const CELL_BASE_CLASS = 'field-cell';
+
+
